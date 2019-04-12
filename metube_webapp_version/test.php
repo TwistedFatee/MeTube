4699ab2ad4381e4cc5c784f50deb6d90a0eb3cad
Ying Cai <cai7@g.clemson.edu>
2019-04-12 14:31:53 -0400

    thumbs

--- metube_webapp_version/test.php
+++ metube_webapp_version/test.php
@@ -1,31 +1,24 @@
 <?php
-ini_set('session.save_path','/home/cai7/temp');
-session_start();
+
 include_once "function.php";
 
-if(isset($_SESSION['userid']) && isset($_SESSION['randomstring'])){
-	$username=$_SESSION['username'];
-	$userid=$_SESSION['userid'];
-	$randomstring=$_SESSION['randomstring'];
+$q="select * from media where type LIKE 'video%'";
+$r=mysql_query($q) or die("".mysql_error());
+
+while($result_row=mysql_fetch_assoc($r)){
 	
-	$start=$_SESSION['start'];
-	
-	$loginrequired=requirelogin($userid, $randomstring, $start);
-	
-	if($loginrequired!=0){
-		header('Location:require_login.php');
-	}
-	
-	$q="select * from account where userid='$userid'";
-	$r = mysql_query($q) or die("Cannot query account.".mysql_error());
-	$result = mysql_fetch_assoc($r);
-	$oldphone=$result['phone'];
-	$oldemail=$result['email'];
-	echo $oldphone;
-	echo $oldemail;
+							$video = $result_row['filepath'].$result_row['filename'];
+							
+							$thumbnail = "uploads/thumbs/".$result_row['mediaid'].".jpg";
+							$cmd="/usr/bin/ffmpeg -deinterlace -an -ss 1 -i ".$video." -t 1 -r 1 -y -vcodec mjpeg -f mjpeg ".$thumbnail." 2>&1";
+							
+							$output = `$cmd`;
+							echo "<pre>$output</pre>";
+							chmod($thumbnail, 0644);
+ 
+						
 }
-else
-{
-	header('Location:require_login.php');
-}
+
+
+
 ?>
